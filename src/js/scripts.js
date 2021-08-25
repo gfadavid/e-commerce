077be0b9f2a88d5a28465f3aa59538a029e4e61f
Ángel David González Flores <gfadavid240@gmail.com>
2021-08-25 16:41:12 -0400

    Estructura definida para las "Ofertas destacadas".

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,26 @@
 // CAROUSEL
-$('.category-carousel').slick({
-  infinite: true,
-  slidesToShow: 6,
-  slidesToScroll: 6
+$('.top-offers').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
 });
