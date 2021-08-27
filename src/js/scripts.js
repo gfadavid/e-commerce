2fdd718fa7f87b2c2c09f9bd6d735d3dba80dc2a
Ángel David González Flores <gfadavid240@gmail.com>
2021-08-27 15:22:12 -0400

    Sección imagen del producto lista para ambas vistas.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -22,3 +22,21 @@ $('.top-offers__carousel').slick({
     }
   ]
 });
+
+ 
+// PRODUCT
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: '.slider-for',
+  focusOnSelect: true
+});
